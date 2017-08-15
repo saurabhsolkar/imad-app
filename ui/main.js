1a9d3edb37f808f8af3ae931ce5718a76222ce81
saurabhsolkar <30970517+saurabhsolkar@users.noreply.github.com>
2017-08-15 14:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,6 @@
  console.log('Loaded!');
 
-var button=document.getElementById("counter");
-button.onclick=function(){
-var request=XMLHttpRequest()
 
-request.onreadystatechange=function(){
-    if(request.readyState==XMLHttpRequest.DONE){
-        if(request.state==200){
-          var counter=  request.responseText;
-           counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
-        }
-    }
-};
-  request.open('GET',"http://saurabhsolkar4.imad.hasura-app.io/counter",true);
-  request.send(null);
-};
 var img=document.getElementById("two");
 img.onclick=function(){
     img.style.marginLeftt="100px";
