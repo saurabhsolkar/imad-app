348b37d3a40e6429a02970ac90700cfe751efdfd
saurabhsolkar <30970517+saurabhsolkar@users.noreply.github.com>
2017-08-15 09:04:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 
 
-app.get('/ui/index.html', function (req, res) {
+app.get('index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', ' index.html'));
 });
 
