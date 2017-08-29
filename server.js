66938e9a27f7f83178d8456168b2f56059839495
saurabhsolkar <30970517+saurabhsolkar@users.noreply.github.com>
2017-08-29 10:24:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/imad-app/myvideo.mp4', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 81;
+var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
