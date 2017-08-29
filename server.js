25184bf49a7856f805d83c0ec438bd9f99ad6966
saurabhsolkar <30970517+saurabhsolkar@users.noreply.github.com>
2017-08-29 11:04:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.get('/test', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter',function(req,res){
+app.get('/cnt',function(req,res){
     counter=counter+1;
-    pool.query("insert into counter(counterv) values ($1)",[counter],function (err, result) {
+    pool.query("insert into counter(cnt1) values ($1)",[counter],function (err, result) {
                             if (err) {
                                 res.status(500).send(err.toString());
                             } else {
